1cdcaec1ffef68ca5f46fd3d618856783a1d0115
Christian Orsini <kairostech01@gmail.com>
2022-10-14 12:08:58 +0200

    minor fixes

--- appdaemon/apps/notification.py
+++ appdaemon/apps/notification.py
@@ -8,10 +8,12 @@ class Notification(hass.Hass):
 
 
     def notification(self, event_name, data, kwargs):
-
-        entity=data["entity"]
         ncode=data["ncode"]
         notification_type=data["type"]
+        if ncode =="HEATING_SENSOR_BATTERY_LOW":
+            self.heatingSensorLowBattery(data)
         self.log("Sending the notification", level="INFO")
-        entity=entity.split(".")[1].split("_")[0].upper()
+
+    def heatingSensorLowBattery(self, data):
+        entity= data["entity"].split(".")[1].split("_")[0].upper()
         self.notify("{} battery is low".format(entity), name="persistent_notification")
