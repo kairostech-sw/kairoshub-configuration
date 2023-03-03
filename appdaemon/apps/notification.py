db13a1c1750214a7dfcadca8118487d38a774e43
Christian Orsini <kairostech01@gmail.com>
2023-03-03 13:42:05 +0100

    ref #7 appdaemon

--- appdaemon/apps/notification.py
+++ appdaemon/apps/notification.py
@@ -4,15 +4,66 @@ import hassapi as hass
 KAIROSHUB_SYSTEM_CODE               = "input_text.system_code"
 file="./kairoshubNotification.json"
 noty_message={
-    "HEATING_ON":"Impianto di riscaldamento acceso correttamente.",
-    "HEATING_ON_ERROR":"Si è verificato un problema nell'accensione dell'impianto di riscaldamento. Verifica l'accensione della caldaia, se il problema persiste contattare l'assistenza.",
-    "HEATING_VALVES_CLOSED":"Sembra che le teste termostatiche dei termosifoni non siano raggiungibili dall'impianto. Verificare la carica delle teste, se cariche e il problema persiste contattare l'assistenza.",
-    "HEATING_OFF":"Impianto di riscaldamento spento correttamente.",
-    "HEATING_OFF_ERROR":"Si è verificato un problema nello spegnimento dell'impianto di riscaldamento. Verifica l'accensione della caldaia, se il problema persiste contattare l'assistenza.",
-    "HEATING_SENSOR_BATTERY_LOW": "La testa termostatica #ENTITY# si sta scaricando. Collegala ad un carica batterie oppure ad una Powerbank. \n\nPuoi ricoscere la testa termostatica dal nome applicato nella parte sottostante.",
-    "TH_SENSOR_BATTERY_LOW": "Il sensore della temperatura e umidità #ENTITY# si sta scaricando. Sostituire le pile nel retro del sensore. \n\nPuoi ricoscere il sensore dal nome applicato nella parte retrostante.",
-    "ROLLERS_OPENED": "Le tapparelle sono state aperte correttamente.",
-    "ROLLERS_CLOSED": "Le tapparelle sono state chiuse correttamente."
+  "HEATING_ON":{
+    "label": "Riscaldamento Acceso",
+    "message":"Impianto di riscaldamento acceso correttamente."
+  },
+  "HEATING_ON_ERROR":{
+    "label": "Errore Riscaldamento",
+    "message":"Si è verificato un problema nell'accensione dell'impianto di riscaldamento. Verifica l'accensione della caldaia, se il problema persiste contattare l'assistenza."
+  },
+  "HEATING_VALVES_CLOSED":{
+    "label": "Errore Valvole",
+    "message":"Sembra che le teste termostatiche dei termosifoni non siano raggiungibili dall'impianto. Verificare la carica delle teste, se cariche e il problema persiste contattare l'assistenza."
+    },
+  "HEATING_OFF":{
+    "label": "Riscaldamento Spento",
+    "message":"Impianto di riscaldamento spento correttamente."
+    },
+  "HEATING_OFF_ERROR":{
+    "label": "Errore Riscaldamento",
+    "message":"Si è verificato un problema nello spegnimento dell'impianto di riscaldamento. Verifica l'accensione della caldaia, se il problema persiste contattare l'assistenza."
+    },
+  "HEATING_SENSOR_BATTERY_LOW": {
+    "label": "Batteria Quasi Scarica Testa Termostatica #ENTITY#",
+    "message":"La testa termostatica #ENTITY# si sta scaricando. Collegala ad un carica batterie oppure ad una Powerbank. \n\nPuoi ricoscere la testa termostatica dal nome applicato nella parte sottostante."
+    },
+  "TH_SENSOR_BATTERY_LOW": {
+    "label": "Batteria Quasi Scarica Sensore Umidità #ENTITY#",
+    "message":"Il sensore della temperatura e umidità #ENTITY# si sta scaricando. Sostituire le pile nel retro del sensore. \n\nPuoi ricoscere il sensore dal nome applicato nella parte retrostante."
+    },
+  "ROLLERS_OPENED": {
+    "label": "Tapparelle Aperte",
+    "message":"Le tapparelle sono state aperte correttamente."
+    },
+  "ROLLERS_CLOSED": {
+    "label": "Tapparelle Chiuse",
+    "message":"Le tapparelle sono state chiuse correttamente."
+    },
+  "LIGHTS_ON": {
+    "label": "Luci Accese",
+    "message": "Le luci sono state accese correttamente."
+  },
+  "LIGHTS_OFF": {
+    "label": "Luci Spente",
+    "message": "Le luci sono state spente correttamente."
+  },
+  "SCENE_NIGHT_ON": {
+    "label": "Scenario Notte",
+    "message": "Lo scenario Notte è ora attivo."
+  },
+  "SCENE_NIGHT_OFF": {
+    "label": "Scenario Notte",
+    "message": "Lo scenario Notte non è più attivo."
+  },
+  "SCENE_DAY_ON": {
+    "label": "Scenario Giorno",
+    "message": "Lo scenario Giorno è ora attivo"
+  },
+  "SCENE_DAY_OFF": {
+    "label": "Scenario Giorno",
+    "message": "Lo scenario Giorno non è più attivo"
+  }
 }
 
 class Notification(hass.Hass):
@@ -53,10 +104,8 @@ class Notification(hass.Hass):
 
     def dispatchNotification(self, notificationToSend):
 
-        if notificationToSend["sender"] == "HUB":
-            self.log("hub notification placeholer")
-        elif notificationToSend["sender"] == "*" or notificationToSend["sender"] != "":
-            self.log("hub notification placeholer")
+        self.sendHubNotification(notificationToSend)
+        if notificationToSend["sender"] == "*" or notificationToSend["sender"] != "" or notificationToSend["sender"] != "HUB":
 
             #removing attributes
             notificationToSend.pop("severity", None)
@@ -68,7 +117,7 @@ class Notification(hass.Hass):
 
     def getMessage(self, code, entityRef):
 
-        message = noty_message[code]
+        message = noty_message[code]["message"]
 
         if not message == "":
             if "#ENTITY#" in message and entityRef != "":
@@ -105,12 +154,31 @@ class Notification(hass.Hass):
     def pushMessage(self, event_name, data, kwargs):
 
         self.log("Pushing message file", level="INFO")
-        jsonData=data["data"]["technicalMessage"]
 
         try:
+            jsonData=data["data"]["technicalMessage"]
             with open(file, "w+") as f:
                 json.dump(jsonData, f)
+        except FileNotFoundError:
+            self.log("File not found", level="WARNING")
+        #     self.log("Requesting notification message file to the cloud", level="INFO")
+        #     # eventData = {
+        #     #     "eventType" : "NOTIFICATION_MESSAGE_REQ",
+        #     #     "sender" : self.systemCode,
+        #     #     "message" : "NOTIFICATION MESSAGE REQUEST"
+        #     # }
+
+        #     # self.fire_event("HAKAFKA_PRODUCER_PRODUCE", topic="TECHNICAL", message=eventData)
         except Exception:
             raise
 
         self.log("File pushed", level="INFO")
+
+    def sendHubNotification(self, notification):
+        self.log("Sending HUB notification")
+        self.log(notification)
+        code = notification["eventType"]
+        label = noty_message[code]["label"]
+
+        self.set_state("input_text.notify", state=label)
+        self.turn_on("input_boolean.notification_to_read")
